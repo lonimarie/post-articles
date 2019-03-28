e579768e139ab5ae8f6531b0c6630a4882e22229
Loni Wooldridge <wooldrlm@kodiak.me>
2019-03-28 14:01:34 -0400

    deploy2

---
+++ client/build/precache-manifest.8be2da5ba577bd571f429779feaca096.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = [
+  {
+    "revision": "1e80540b18557641663e09dd5db19c40",
+    "url": "/static/media/city.1e80540b.jpg"
+  },
+  {
+    "revision": "edcb5dbe84d7a4e3fdbc",
+    "url": "/static/css/main.440890f8.chunk.css"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "74739961a09f5f9ffa0eeb8acebdfc73",
+    "url": "/static/media/business.74739961.jpg"
+  },
+  {
+    "revision": "596c70110484ad18ce46",
+    "url": "/static/js/2.46a1c72a.chunk.js"
+  },
+  {
+    "revision": "f768d7886dfb04abf82763736eb695e1",
+    "url": "/static/media/other.f768d788.jpg"
+  },
+  {
+    "revision": "5c8e636e24fea241b2e92117991a0348",
+    "url": "/static/media/boy.5c8e636e.jpg"
+  },
+  {
+    "revision": "fecc1611e13b25035e84da3cfdf42278",
+    "url": "/static/media/animal.fecc1611.jpg"
+  },
+  {
+    "revision": "4bf341c557a8167f8466d853c2a515b5",
+    "url": "/static/media/beach.4bf341c5.jpg"
+  },
+  {
+    "revision": "fa4f5ad66e4f42e094dc426f91c94bf6",
+    "url": "/static/media/cars.fa4f5ad6.jpg"
+  },
+  {
+    "revision": "edcb5dbe84d7a4e3fdbc",
+    "url": "/static/js/main.baf95872.chunk.js"
+  },
+  {
+    "revision": "8999912694b248ab08ef1f205254d2c8",
+    "url": "/static/media/food.89999126.jpg"
+  },
+  {
+    "revision": "9a7f554486aecd75d7c71f39d11a4bd5",
+    "url": "/static/media/health.9a7f5544.jpg"
+  },
+  {
+    "revision": "e642fe0f5d3b8e34491edb7b1ebb209a",
+    "url": "/static/media/landscape.e642fe0f.jpg"
+  },
+  {
+    "revision": "7057406355b332bda5ca311a285034a1",
+    "url": "/static/media/music.70574063.jpg"
+  },
+  {
+    "revision": "b5d2265c829d61914a981b4b6bf4faf0",
+    "url": "/static/media/sports.b5d2265c.jpg"
+  },
+  {
+    "revision": "ef0379b7d20b5347e99e16cc5f6bed9e",
+    "url": "/static/media/technology.ef0379b7.jpg"
+  },
+  {
+    "revision": "b7c06d6eaff7b70ab0e866374c71cb3d",
+    "url": "/static/media/weather.b7c06d6e.jpg"
+  },
+  {
+    "revision": "596c70110484ad18ce46",
+    "url": "/static/css/2.8ad26679.chunk.css"
+  },
+  {
+    "revision": "4ee131d3202a0e437e1a790947a13c29",
+    "url": "/index.html"
+  }
+];
