75f325a76b97a1822e97f34e3fddfb445e2dd265
Loni Wooldridge <wooldrlm@kodiak.me>
2019-04-01 15:28:57 -0400

    deployment

--- server.js
+++ server.js
@@ -151,9 +151,9 @@ app.use('/articles', articleRoutes);
     // Serve any static files
     app.use(express.static(path.join(__dirname, 'client/build')));
     // Handle React routing, return all requests to React app
-    // app.get('*', function(req, res) {
-    //   res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
-    // });
+     app.get('*', function(req, res) {
+       res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+     });
 //   }
 
 
